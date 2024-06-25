b77475ecb7af6f36dfa928442ee2d1d51816ab33
yarden <yarden.tulchinsky@gmail.com>
2024-06-25 12:44:33 +0300

    completing the web scraping from billabord. getting the html code and with beatiful soup extract the songs name and singers name. entering each song and singer into a list of dicts.

---
+++ main.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+#getting the year of choice to get the top songs of that time
+year_choice = input('what year you would like to travel to. type the date in YYY-MM-DD format: ')
+
+#Sending a Get request to to get the HTML code of the website
+URL =f'https://www.billboard.com/charts/hot-100/{year_choice}'
+response = requests.get(URL)
+response.raise_for_status()
+data = response.text
+
+with open('billabord.html', encoding='utf-8') as file:
+    data = file.read()
+
+#Start of Web Scraping with Beautiful Soup
+soup = BeautifulSoup(data,'html.parser')
+
+#Getting the Info And Tags that includes the Song name and the Singer Name
+songs_html = soup.find_all(name='h3', id="title-of-a-story", class_="a-no-trucate")
+singers_html = soup.find_all(name='span', class_='a-no-trucate')
+
+#Getting the Texts of the Song and Singer, on the way fixing the text by splitting it
+songs_list_fix = [song.getText().split('\t')[9] for song in songs_html]
+singers_list_fix = [singer.getText().split('\t')[2].split('\n')[0] for singer in singers_html]
+
+#Build a list with Dicts each has keys of Singer and Song
+song_list = [{'singer':singers_list_fix[n],'song_name':songs_list_fix[n]} for n in range(len(songs_list_fix)-1)]
